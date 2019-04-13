b8a05ca3c2c55b9d053b4ee3f0cd9928b48925a8
Nazeem <muhammed.nazeem@experionglobal.com>
2019-04-13 13:43:44 +0530

    PG - Testing Live

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
-	$API_URL = "https://muthootlive.azure-mobile.net/api/";
+	$API_URL = "https://muthootdev.azure-mobile.net/api/";
 	
-	$accesstoken = "ZYHWiOqBYiHORTVkmNarVeTrYHTLfp38"; // CURL
+	$accesstoken = "JODKFspBxMyxtIrHIqVSEExBzKfGlR50"; // CURL
 
 	$SECURE_SECRET = "154E0E8E5C53404CDB5CF31C7EA9BD1D";  // card transaction
 
