68267cd0ab886b81bbdfa838373919f4ae371127
Nazeem <muhammed.nazeem@experionglobal.com>
2020-10-31 15:33:10 +0530

    PGP Test

--- pgp_test.php
+++ pgp_test.php
@@ -1,6 +1,9 @@
 <?php
+  echo "**";
   $res = gnupg_init();
+  echo "000";
   gnupg_addencryptkey($res,"8660281B6051D071D94B5B230549F9DC851566DC");
   $enc = gnupg_encrypt($res, "just a test");
-  echo $enc;
+  echo "--";
+  print_r($enc);
 ?>
