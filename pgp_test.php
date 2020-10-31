c8ad13207cbeca472184190ac9d7ab605b0143c8
Nazeem <muhammed.nazeem@experionglobal.com>
2020-10-31 18:34:06 +0530

    PGP Test

--- pgp_test.php
+++ pgp_test.php
@@ -1,6 +1,13 @@
 <?php
 $gpg = new gnupg();
-$gpg -> addencryptkey("8660281B6051D071D94B5B230549F9DC851566DC");
-$enc = $gpg -> encrypt("just a test");
+$info = $gpg -> import($keydata);
+print_r($info);
+
+echo "=====================================================";
+
+$res = gnupg_init();
+gnupg_addencryptkey($res,"8660281B6051D071D94B5B230549F9DC851566DC");
+$enc = gnupg_encrypt($res, "just a test");
+echo "Encryption is: ";
 echo $enc;
 ?>
