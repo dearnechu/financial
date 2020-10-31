9eb4e3014fc876654b05058ba12ff8e4fb007269
Nazeem <muhammed.nazeem@experionglobal.com>
2020-10-31 20:57:55 +0530

    PGP Test

--- pgp_test.php
+++ pgp_test.php
@@ -8,4 +8,9 @@ $PublicKey = $GnuPG->import($PublicData);
 $PrivateKey = $GnuPG->import($PrivateData);
 
 echo 'Public Key : ',$PublicKey['fingerprint'],' & Private Key : ',$PrivateKey['fingerprint'];
+
+gnupg_addencryptkey($res, $PublicKey['fingerprint']);
+$enc = gnupg_encrypt($res, "just a test");
+echo $enc;
+
 ?>
