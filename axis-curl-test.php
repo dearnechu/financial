2baf750f6d3bf7de1c7127c09331a91e376503d6
Muhammed Nazeem K <muhammed.nazeem@experionglobal.local>
2021-04-01 18:49:41 +0530

    Add sign and encrypt

--- axis-curl-test.php
+++ axis-curl-test.php
@@ -6,6 +6,7 @@
   $PublicData = file_get_contents('key/msnl_uat.pkr');
   $PublicKey = $GnuPG->import($PublicData);
   $GnuPG->addencryptkey($PublicKey['fingerprint']);
+  $GnuPG->addsignkey($PublicKey['fingerprint']);
 
   if (!file_exists('logs/reverse/test/' . date('Ymd'))) {
       mkdir('logs/reverse/test/' . date('Ymd'), 0777, true);
@@ -77,7 +78,7 @@
   curl_setopt($ch, CURLOPT_HTTPHEADER,array('Cache-Control: no-cache'));
 
   $jsondata = json_encode($payment_array);
-  $enc = $GnuPG->encrypt($jsondata);
+  $enc = $GnuPG->encryptsign($jsondata);
   fwrite($fp, $jsondata);
   fwrite($fp, PHP_EOL . $enc);
   
