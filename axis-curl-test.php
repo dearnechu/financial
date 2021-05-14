d34b3abc8aac21e29e1374e97d126ce64b10e865
Muhammed Nazeem K <muhammed.nazeem@experionglobal.local>
2021-05-14 11:40:36 +0530

    MSNL Production key

--- axis-curl-test.php
+++ axis-curl-test.php
@@ -1,57 +1,18 @@
 <?php
   error_reporting(0);
   session_start();
-  try {
-      $gpg = new gnupg();
-      // throw exception if error occurs
-
-      $PublicData = file_get_contents('key/msnl_uat.pkr');
-      $PublicKey = $gpg->import($PublicData);
-      $gpg->seterrormode(gnupg::ERROR_EXCEPTION);
-      $gpg->addencryptkey($PublicKey['fingerprint']);
-
-      $privateData = file_get_contents('key/private-muthoot.pkr');
-      $privateKey = $gpg->import($privateData);
-
-      echo $privateKey['fingerprint'];
-      
-      $cipher_text = $gpg->encrypt('This is a test message');
-      echo $cipher_text;
-
-
-      $gpg->addsignkey($privateKey['fingerprint'], '@Magento318');
-      $signed = $gpg->encryptsign("just a test");
-      echo "<br>".$signed;
-
-  } catch (Exception $e) {
-      // restore the envelope
-      echo "Failed";
-      print_r($e);
-      // re-throw the exception
-  }
-
-  exit;
 
   $GnuPG = new gnupg();
-  $PublicData = file_get_contents('key/msnl_uat.pkr');
+  $PublicData = file_get_contents('key/msnl_prod.pkr');
   $PublicKey = $GnuPG->import($PublicData);
   $GnuPG->addencryptkey($PublicKey['fingerprint']);
-  
 
-  if (!file_exists('logs/reverse/test/' . date('Ymd'))) {
-      mkdir('logs/reverse/test/' . date('Ymd'), 0777, true);
+  if (!file_exists('logs/reverse/' . date('Ymd'))) {
+      mkdir('logs/reverse/' . date('Ymd'), 0777, true);
   }
   $uneque_refrence_number = date('YmdHis');
-  $fp = fopen('logs/reverse/test/' . date('Ymd') .'/'. $uneque_refrence_number . '.txt', 'w');
+  $fp = fopen('logs/reverse/' . date('Ymd') .'/'. $uneque_refrence_number . '.txt', 'w');
 
-/*
-  print_r($_SESSION['GetBankDetailsByCustomerId']);
-  print_r($_SESSION['GetGoldLoanDetailsWeb']);
-  
-  echo "<br>-" . $_SESSION['customer_email'];
-  echo "<br>-" .  $_SESSION['customer_phone'];
-  echo "<br>-" .  $_SESSION['loanAmount'];
-*/
   $payment_array['RECORD']['PAYMENT_DETAILS'][0]['PAYMENTS']['API_VERSION'] = '1';
   $payment_array['RECORD']['PAYMENT_DETAILS'][0]['PAYMENTS']['CORP_CODE'] = 'DEMOCORP279';
   $payment_array['RECORD']['PAYMENT_DETAILS'][0]['PAYMENTS']['CMPY_CODE'] = '029010100347068';
@@ -69,8 +30,6 @@
 
   
   $payment_array['RECORD']['PAYMENT_DETAILS'][0]['PAYMENTS']['VALUE_DATE'] = date('d-m-Y'); // '21-09-2020';
-  // $payment_array['RECORD']['PAYMENT_DETAILS'][0]['PAYMENTS']['RUN_IDENTIFICATION'] = 'SBK0820A0003197_6946';
-  // $payment_array['RECORD']['PAYMENT_DETAILS'][0]['PAYMENTS']['FILE_NAME'] = 'SBK0820A0003197_6946';
 
   $payment_array['RECORD']['PAYMENT_DETAILS'][0]['PAYMENTS']['BENE_CODE'] = $_SESSION['GetGoldLoanDetailsWeb']['customerCode']; // '6946';
   $payment_array['RECORD']['PAYMENT_DETAILS'][0]['PAYMENTS']['BENE_NAME'] = $_SESSION['GetBankDetailsByCustomerId']['accountHolder']; // 'MUHAMMED NAZEEM'
@@ -82,39 +41,39 @@
 
   $payment_array['RECORD']['PAYMENT_DETAILS'][0]['PAYMENTS']['PRODUCT_CODE'] = 'pa';
   $payment_array['RECORD']['PAYMENT_DETAILS'][0]['PAYMENTS']['BATCH_ID'] = 'MUTHOOT_MSNL';
+
+  // $payment_array['RECORD']['PAYMENT_DETAILS'][0]['PAYMENTS']['BENE_ADDR_1'] = 'arun nagar,Pimpalgaon,Pimpalgaon Bk.,Jalgaon,Pachora,Maharashtra,424203,';
+  // $payment_array['RECORD']['PAYMENT_DETAILS'][0]['PAYMENTS']['BENE_ADDR_2'] = 'test';
+  // $payment_array['RECORD']['PAYMENT_DETAILS'][0]['PAYMENTS']['BENE_ADDR_3'] = '';
+  // $payment_array['RECORD']['PAYMENT_DETAILS'][0]['PAYMENTS']['BENE_CITY'] = 'Jalgaon';
+  // $payment_array['RECORD']['PAYMENT_DETAILS'][0]['PAYMENTS']['BENE_STATE'] = 'Maharashtra';
+  // $payment_array['RECORD']['PAYMENT_DETAILS'][0]['PAYMENTS']['BENE_PINCODE'] = '424203';
   
   $payment_array['RECORD']['PAYMENT_DETAILS'][0]['PAYMENTS']['CORP_EMAIL_ADDR'] = 'support@muthootenterprises.com';
   $payment_array['RECORD']['PAYMENT_DETAILS'][0]['PAYMENTS']['BENE_MOBILE_NO'] = $_SESSION['customer_phone'];
   $payment_array['RECORD']['PAYMENT_DETAILS'][0]['PAYMENTS']['BENE_EMAIL_ADDR1'] = $_SESSION['customer_email'];
+  // $payment_array['RECORD']['PAYMENT_DETAILS'][0]['PAYMENTS']['BENE_EMAIL_ADDR2'] = 'test@axisbank.com';
   
 
-  $ch = curl_init('https://qah2h.axisbank.co.in/RESTAdapter/AxisBank/Muthoot36/Pay');
+  $ch = curl_init('https://h2h.axisbank.co.in/RESTAdapter/AxisBank/Muthoot36/Pay');
   curl_setopt($ch, CURLOPT_TIMEOUT, 30); //timeout after 30 seconds
   curl_setopt($ch, CURLOPT_PORT, 443);
   curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
   curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
   curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
   curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_ANY);
-  curl_setopt($ch, CURLOPT_USERNAME, "corpuser");
-  curl_setopt($ch, CURLOPT_USERPWD, "axiscorpcon1!");
+  curl_setopt($ch, CURLOPT_USERNAME, "corp_muthoot36");
+  curl_setopt($ch, CURLOPT_USERPWD, "axiscorpcon1");
   curl_setopt($ch, CURLOPT_HTTPHEADER,array('Cache-Control: no-cache'));
-  
-  $privateData = file_get_contents('key/private-muthoot.pkr');
-  $privateKey = $GnuPG->import($privateData);
-
-  fwrite($fp, PHP_EOL . 'Sign Status: ' . $GnuPG->addsignkey($privateKey['fingerprint']));
-  $signed = $GnuPG->sign($jsondata);
-  fwrite($fp, PHP_EOL . 'Signed: ' . $signed);
 
   $jsondata = json_encode($payment_array);
-  fwrite($fp, PHP_EOL . 'JSON: ' . $jsondata);
-
-  $enc = $GnuPG->encryptsign($jsondata);
-  fwrite($fp, PHP_EOL . 'Enc: ' . $enc);
+  $enc = $GnuPG->encrypt($jsondata);
+  fwrite($fp, $jsondata);
+  fwrite($fp, PHP_EOL . $enc);
   
   curl_setopt($ch, CURLOPT_POSTFIELDS, $enc ); 
   curl_setopt($ch, CURLOPT_HTTPHEADER,array('Content-Type: text/plain;charset=UTF-8')); 
-  curl_setopt($ch, CURLOPT_HTTPHEADER,array('Authorization: Basic Y29ycHVzZXI6YXhpc2NvcnBjb24xIQ==')); 
+  curl_setopt($ch, CURLOPT_HTTPHEADER,array('Authorization: Basic Y29ycF9tdXRob290MzY6YXhpc2NvcnBjb24x')); 
      
   $server_output = curl_exec($ch);
   $errors = curl_error($ch);
@@ -124,46 +83,5 @@
   fwrite($fp, PHP_EOL . 'Server Output: ' . $server_output);
   fwrite($fp, PHP_EOL . 'Errors: ' . $errors);
   fwrite($fp, PHP_EOL . 'Response: ' . $response);
-
-
-  //   {
-  //     "Request": {
-  //        "CORP_CODE": "DEMOCORP279",
-  //        "PAY_DOC_NUMBER": [
-  //           $uneque_refrence_number,
-  //        ]
-  //     }
-  //  }
-
-  $enq_array['Request']['CORP_CODE'] = 'DEMOCORP279';
-  $enq_array['Request']['PAY_DOC_NUMBER'] = [$uneque_refrence_number];
-  $jsondata = json_encode($enq_array);
-  fwrite($fp, PHP_EOL . 'Enq req: ' . $jsondata);
-  $enc = $GnuPG->encrypt($jsondata);
-  fwrite($fp, PHP_EOL . 'Enc Enq: ' . $enc);
-
-  $ch = curl_init('https://qah2h.axisbank.co.in/RESTAdapter/AxisBank/Muthoot36/Pay/Enq');
-  curl_setopt($ch, CURLOPT_TIMEOUT, 30); //timeout after 30 seconds
-  curl_setopt($ch, CURLOPT_PORT, 443);
-  curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
-  curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
-  curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-  curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_ANY);
-  curl_setopt($ch, CURLOPT_USERNAME, "corpuser");
-  curl_setopt($ch, CURLOPT_USERPWD, "axiscorpcon1!");
-  curl_setopt($ch, CURLOPT_HTTPHEADER,array('Cache-Control: no-cache'));
-
-  curl_setopt($ch, CURLOPT_POSTFIELDS, $enc ); 
-  curl_setopt($ch, CURLOPT_HTTPHEADER,array('Content-Type: text/plain;charset=UTF-8')); 
-  curl_setopt($ch, CURLOPT_HTTPHEADER,array('Authorization: Basic Y29ycHVzZXI6YXhpc2NvcnBjb24xIQ==')); 
-
-  $server_output = curl_exec($ch);
-  $errors = curl_error($ch);
-  $response = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-
-  curl_close($ch);
-  fwrite($fp, PHP_EOL . 'Enq Server Output: ' . $server_output);
-  fwrite($fp, PHP_EOL . 'Enq Errors: ' . $errors);
-  fwrite($fp, PHP_EOL . 'Enq Response: ' . $response);
   fclose($fp);
 ?>
