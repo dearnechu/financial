ce8ab11867b3afe9738827fe14a5513a68481e2a
Muhammed Nazeem K <muhammed.nazeem@experionglobal.local>
2021-05-21 19:04:03 +0530

    Socket - Telnet

--- socket.php
+++ socket.php
@@ -1,8 +1,9 @@
 <?php
 $fp = fsockopen("s2a.axisbank.co.in", 443, $errno, $errstr, 30);
 if (!$fp) {
-    echo "---"."$errstr ($errno)<br />\n";
+    echo "$errstr ($errno)<br />\n";
 } else {
+    echo "Coming..";
     $out = "GET / HTTP/1.1\r\n";
     $out .= "Host: s2a.axisbank.co.in\r\n";
     $out .= "Connection: Close\r\n\r\n";
