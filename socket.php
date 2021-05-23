db56e99fa60834fbf4c3f45703699e3dcd4d484f
Muhammed Nazeem K <muhammed.nazeem@experionglobal.local>
2021-05-23 20:08:05 +0530

    Socket test

--- socket.php
+++ socket.php
@@ -15,9 +15,9 @@ if (($sock = socket_create(AF_INET, SOCK_STREAM, SOL_TCP)) === false) {
     echo "socket_create() failed: reason: " . socket_strerror(socket_last_error()) . "\n";
 }
 
-if (socket_bind($sock, $address, $port) === false) {
-    echo "socket_bind() failed: reason: " . socket_strerror(socket_last_error($sock)) . "\n";
-}
+// if (socket_bind($sock, $address, $port) === false) {
+//     echo "socket_bind() failed: reason: " . socket_strerror(socket_last_error($sock)) . "\n";
+// }
 
 if (socket_listen($sock, 5) === false) {
     echo "socket_listen() failed: reason: " . socket_strerror(socket_last_error($sock)) . "\n";
