3bb4e5f69b13d98b32f22f1d5d91ac181609e5a6
Muhammed Nazeem K <muhammed.nazeem@experionglobal.local>
2021-10-09 11:10:12 +0530

    Change service provide

--- sms.php
+++ sms.php
@@ -2,9 +2,9 @@
 	$input = file_get_contents('php://input');
 	if($input){
 		$input_array = json_decode($input, true);
-		$url = "https://sms.xpresssms.in/api/api.php?ver=1&mode=1&action=push_sms&type=1";
-		$url .= "&route=2&login_name=muthootmercantile&api_password=f38ad42506766914d5d7&";
-		$url .= "message=".str_replace(" ", "%20", $input_array['message']); 
+		$url = "http://sapteleservices.com/SMS_API/sendsms.php?username=muthootgroup&password=muthoot12345&sendername=MUTHOT&routetype=1";
+
+		$url .= "&message=".str_replace(" ", "%20", $input_array['message']); 
 		if ($input_array['email']) {
 			$url .= ".%20Email:%20".$input_array['email']."%20";	
 		}
@@ -12,8 +12,8 @@
 			$url .= "Password:%20".$input_array['password'];
 		}
 		$url .= ".%20MUTHOOT%20MERCANTILE%20LTD";
-		$url .= "&number=".$input_array['mobile']."&sender=MUTHOT";
-		$url .= "&template_id=".$input_array['template_id'];
+		$url .= "&mobile=".$input_array['mobile']."&sender=MUTHOT";
+		echo $url .= "&template_id=".$input_array['template_id'];
 		$ch = curl_init();
 		curl_setopt($ch, CURLOPT_URL, $url);
 		$server_output = curl_exec ($ch);
