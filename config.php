406dfcdbb22aa5f8eebeba63dbca59292bd36353
exp-nazeem <muhammed.nazeem@experionglobal.com>
2022-07-06 11:40:51 +0530

    Added database credentials

--- config.php
+++ config.php
@@ -5,4 +5,8 @@
 
 	$SECURE_SECRET = "154E0E8E5C53404CDB5CF31C7EA9BD1D";  // card transaction
 
+	$db_servername = "localhost";
+	$db_username = "muthoot";
+	$db_password = "H7TVsz4FD7aR";
+	$db_database = "muthoot_muthootone";
 ?>
