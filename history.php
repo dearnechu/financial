fda421f0b1b3355db9195e530f0aae63fe675dd1
exp-nazeem <muhammed.nazeem@experionglobal.com>
2022-07-06 17:27:32 +0530

    Fix Session timeout

--- history.php
+++ history.php
@@ -1,9 +1,15 @@
+<?php
+  ini_set("session.cookie_httponly", 1);
+  ini_set('session.cookie_secure', 1);
+  session_start();
+  session_regenerate_id();
+?>
 <!DOCTYPE html>
 <html>
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta http-equiv="refresh" content="1205" />
+    <meta http-equiv="refresh" content="1200" />
     <title>MuthootOne | Loans</title>
     <!-- Tell the browser to be responsive to screen width -->
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
