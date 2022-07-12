4e2c18cfddf4295e9423a26ebf8aba2068922443
exp-nazeem <muhammed.nazeem@experionglobal.com>
2022-07-12 11:51:52 +0530

    Fix Cookie issue

--- home.php
+++ home.php
@@ -1,7 +1,6 @@
 <?php
   ini_set("session.cookie_httponly", 1);
   ini_set('session.cookie_secure', 1);
-  ini_set("session.cookie_samesite", "Lax");
   session_start();
   session_regenerate_id();
 ?>
